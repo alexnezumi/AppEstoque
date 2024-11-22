922a7ef4a1f9105b036306ade33b6fb9f7492dbe
alexnezumi <alexsilva.viana2006@gmail.com>
2024-11-21 21:33:57 -0300

    login smt

--- app/src/main/java/br/com/etecia/appestoque/CadastroActivity.java
+++ app/src/main/java/br/com/etecia/appestoque/CadastroActivity.java
@@ -1,7 +1,6 @@
 package br.com.etecia.appestoque;
 
 import android.os.Bundle;
-import android.os.Handler;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -9,18 +8,17 @@ import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
 
-public class LoginActivity extends AppCompatActivity {
+public class CadastroActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         EdgeToEdge.enable(this);
-        setContentView(R.layout.login_layout);
+        setContentView(R.layout.cadastro_layout);
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
             Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
             return insets;
         });
-
     }
 }
